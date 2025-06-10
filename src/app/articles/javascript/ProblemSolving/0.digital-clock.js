cba0383857d147c61eab6f76b53f3d74b9307ea0
Umer Hussain <umerhussa11n@gmail.com>
2025-06-10 19:26:02 +0100

    improving the clock and using the built in formatting

--- src/app/articles/javascript/ProblemSolving/0.digital-clock.js
+++ src/app/articles/javascript/ProblemSolving/0.digital-clock.js
@@ -3,16 +3,17 @@
 // for 24 hours format its not required for return HH:MM:SS where HH will be 24 hours format.
 function getTime(format) {
   const allowedFormats = ['12hours', '24hours'];
-  if (allowedFormats.find(f => f === format)){
-    if (format === '12hours') {
-      return new Date().toLocaleTimeString('en-GB', 
-        {hour12: true, timeZone: 'Europe/London'}
-      )
-    } else {
-      return new Date().toLocaleTimeString('en-GB', 
-          { hour12: false, timeZone: 'Europe/London'})
+
+  if (allowedFormats.find(f => f === format)) {
+    const is12HourFormat = format === '12hours' ? true : false
+    const dateTimeFormatOptions = {
+      hour12: is12HourFormat,
+      timeZone: 'Europe/London',
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit"
     }
-    return time
+    return new Date().toLocaleTimeString('en-GB', dateTimeFormatOptions)
   } else {
     throw new Error("Only accepatable format are '12hours' and  '24hours' ")
   }
